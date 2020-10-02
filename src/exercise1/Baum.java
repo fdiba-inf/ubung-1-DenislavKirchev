4f995e079d1bde54925dd993394140681b3aecee
DenislavKirchev <denislav_delon@abv.bg>
2020-10-02 05:53:22 +0000

    .

---
+++ src/exercise1/Baum.java
@@ -0,0 +1,13 @@
+package exercise1;
+
+public class Baum{
+
+  public static void main(String [] args){
+    System.out.printl("    *");
+    System.out.printl("   ***");
+    System.out.printl("  *****");
+    System.out.printl(" *******");
+    System.out.printl("*********");
+    System.out.printl("   ***");
+  }
+}
---
+++ src/exercise1/FdibaOutput.java
@@ -0,0 +1,12 @@
+package exercise1;
+
+public class FdibaOutput{
+
+  public static void main(String[]args){
+    System.out.printl("FFFFF DDDDD   II BBBBB    A    ");
+    System.out.printl("F     D    D  II B    B    A A   ");
+    System.out.printl("FFFF  D     D II BBBBB    AAAAA  ");
+    System.out.printl("F     D    D  II B    B  A     A ");
+    System.out.printl("F     DDDDD   II BBBBB  A       A")
+  }
+}
